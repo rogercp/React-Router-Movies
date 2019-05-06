2ddb5585cd67e80c86aa36d466fad196efe5feec
Roger Caballero <rogercp1234@yahoo.com>
2019-05-06 15:16:27 -0700

    refactored code to get rid of the repeated code; created new component in moviecard.js file

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard'
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -33,25 +34,12 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <Link to={`/movies/${movie.id}`}>
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+    
+      <MovieCard movie={movie}/>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
     </Link>
   );
 }
