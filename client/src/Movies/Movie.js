1240673e57c34f04ed354bb8c6a671942cba0a67
Roger Caballero <rogercp1234@yahoo.com>
2019-05-06 14:18:11 -0700

    movies are now clickable and link to their own clicked id

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,8 +11,10 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
-    this.fetchMovie(id);
+  
+  const id = this.props.match.params.id
+  
+  this.fetchMovie(id);
   }
 
   fetchMovie = id => {
