ba7f47549cf9e34a07639c1528a1ffb11e03b150
Roger Caballero <rogercp1234@yahoo.com>
2019-05-06 14:30:24 -0700

    home button functional with Link component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -13,7 +13,6 @@ export default class Movie extends Component {
     // change this line to grab the id passed on the URL
   
   const id = this.props.match.params.id
-  
   this.fetchMovie(id);
   }
 
